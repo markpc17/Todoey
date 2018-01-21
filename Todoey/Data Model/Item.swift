92656c8629b606428186aece898a3ce50d471a55
Mark Carruthers <markpc17@sky.com>
2018-01-21 15:49:03 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mark Carruthers on 21/01/2018.
+//  Copyright © 2018 Mark Carruthers. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
